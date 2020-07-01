081d36ae2784603a999a2b479a86639d5faae201
Mikita Kandratsyeu <nickondr.production@gmail.com>
2020-07-01 17:16:29 +0300

    Working with a component - Chart.jsx

--- src/api/index.js
+++ src/api/index.js
@@ -6,13 +6,26 @@ export const fetchData = async () => {
   try {
     const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
 
-    return {
-      confirmed,
-      recovered,
-      deaths,
-      lastUpdate,
-    };
+    return { confirmed, recovered, deaths, lastUpdate };
   } catch (e) {
-    console.log(e);
+    console.log(`Error: ${e}`);
   }
 };
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    const modifiedData = data.map((dailyData) => {
+      return ({
+        confirmed: dailyData.confirmed.total,
+        deaths: dailyData.deaths.total,
+        date: dailyData.reportDate,
+      });
+    });
+
+    return modifiedData;
+  } catch (e) {
+    console.log(`Error: ${e}`);
+  }
+}
